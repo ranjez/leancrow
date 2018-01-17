ce31da5f233290169039eb0f85ff124ede1c52c1
ranjez <ranjez@gmail.com>
2018-01-17 21:36:44 +0000

    switched to leancrow

--- src/js/app.js
+++ src/js/app.js
@@ -8,3 +8,20 @@ if (window.netlifyIdentity) {
     }
   });
 }
+
+(function () {
+  var hamburger = document.querySelector(".hamburger");
+  // On click
+  hamburger.addEventListener("click", function () {
+    // Toggle class "is-active"
+    hamburger.classList.toggle("is-active");
+    // Do something else, like open/close menu
+    toggleNavigation();
+  });
+
+  function toggleNavigation() {
+    var navigation = document.getElementById('mainNavigation');
+    navigation.classList.contains('hidden') ? navigation.classList.remove('hidden') : navigation.classList.add('hidden');
+  }
+
+})();
