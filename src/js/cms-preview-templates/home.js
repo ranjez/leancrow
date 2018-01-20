cb95fc909bf605c5dc5af0f5e329ace4f3af5877
ranjez <ranjez@gmail.com>
2018-01-20 23:36:14 +0000

    fix import

--- src/js/cms-preview-templates/home.js
+++ src/js/cms-preview-templates/home.js
@@ -5,7 +5,7 @@ import Jumbotron from "./components/jumbotron";
 
 export default class PostPreview extends React.Component {
   render() {
-
+    const {widgetFor} = this.props;
     return <div>
         { widgetFor("body") }
     </div>
